ec5349928f0725b4c94e9272522aaa9eb5a7d2b9
Saad <49029610+saad-khannn@users.noreply.github.com>
2019-09-18 00:24:03 -0400

    Updated buyClearsMap test to work
    The test to call buy which clears the map now works

--- test/paystation/domain/PayStationImplTest.java
+++ test/paystation/domain/PayStationImplTest.java
@@ -261,6 +261,7 @@ public class PayStationImplTest {
         ps.addPayment(5);
         ps.addPayment(10);
         ps.addPayment(25);
+        ps.buy();
         Map total = ps.cancel();
         Map clearMap = new HashMap<>();
         assertEquals(total, clearMap);
